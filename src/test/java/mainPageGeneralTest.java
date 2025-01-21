a3946da18f2ef9e7610d706e0c8e56cd30d0eb2c
AkTIyK123!Q <andrii.omelnytskyi@nure.ua>
2025-01-21 11:40:54 +0200

    test ng

--- src/test/java/mainPageGeneralTest.java
+++ src/test/java/mainPageGeneralTest.java
@@ -1,6 +1,7 @@
-import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 import pages.itemPage;
 import pages.mainPage;
 
